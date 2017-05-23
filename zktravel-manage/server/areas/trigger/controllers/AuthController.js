85edc4f23e48416c122f1b21baed68d7c8dae8d7
wangkehan <shjyh651079151@gmail.com>
2017-05-23 18:43:42 +0800

    login

--- zktravel-manage/server/areas/trigger/controllers/AuthController.js
+++ zktravel-manage/server/areas/trigger/controllers/AuthController.js
@@ -2,16 +2,20 @@ const Controller = require('@local/koa-mvc/Controller');
 const DES = require('@local/des');
 const des = new DES('zouke7788');
 const sessionStore = requireRoot('middleware/sessionStore');
+const codeKey = '/api/auth/code';
+const codeExpriesKey = '/api/auth/code-expries';
 
 module.exports = class AuthController extends Controller {
     async login(){
         const { code, uid } = this.request.query;
-        const key = des.decrypt(code);
-
-        const sessionId = key.split('/')[0];
+        const sessionId = des.decrypt(code).split('/')[0];
         const session = sessionStore.get(sessionId);
-        if(session){
-            session.uid = uid;
+        const now = new Date();
+
+        if(session&&session[codeKey]===code&&session[codeExpriesKey]>=now.valueOf()){
+            session.uid = parseInt(uid)||uid;
+            delete session[codeKey];
+            delete session[codeExpriesKey];
             sessionStore.set(sessionId, session);
         }
     }
