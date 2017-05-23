14f091e912490a176597d394143c7d169e01b401
guanaxing <15236521275@163.com>
2017-05-23 09:58:07 +0800

    update

--- zktravel-manage/client-src/hotel/stock/Vts.vue
+++ zktravel-manage/client-src/hotel/stock/Vts.vue
@@ -59,26 +59,6 @@
 				console.log(this.listData.slice((this.pageNum-1)*this.pageSize,this.pageSize*this.pageNum));
 				return this.listData.slice((this.pageNum-1)*this.pageSize,this.pageSize*this.pageNum);
 
-//				ajax.post('/api/vt-mapping/query').then(json=>{
-//					if(json.code===0){
-//						this.items=json.list;
-//					}
-//				}).then(error=>{
-//					console.log(1);
-//				})
-//
-//				this.listData=this.items.sort(function(a,b){
-//					if(a.id>b.id){
-//						return 1;
-//					}
-//					if(a.id<b.id){
-//						return -1;
-//					}
-//						return 0;
-//				});
-//				console.log(this.listData.slice((this.pageNum-1)*this.pageSize,this.pageSize*this.pageNum));
-//				return this.listData.slice((this.pageNum-1)*this.pageSize,this.pageSize*this.pageNum);
-
 			}
 		},
 		methods:{
