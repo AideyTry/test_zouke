cb8fe37315ae0699e0209d6d6b045d2308076087
wangkehan <shjyh651079151@gmail.com>
2017-05-24 12:37:28 +0800

    query zk/sp hotel

--- zktravel-manage/server/areas/api/logic/SpHotel.js
+++ zktravel-manage/server/areas/api/logic/SpHotel.js
@@ -3,23 +3,37 @@ const mongodb = require('mongodb');
 const { sp_collection_name } = requireRoot('service/config');
 
 module.exports = class SpHotel {
-    async detail(id){
+    async detail(id, type){
+
+        const query = typeof id === 'string' /* _id */? {
+            _id: new mongodb.ObjectId(id)
+        }:{
+            id,
+            supplier: type
+        }
         const db = await dbclient.get();
         const spCollection = await db.collection(sp_collection_name);
 
-        return await spCollection.findOne(
-            {_id: new mongodb.ObjectId(id)}, 
-            {
-                fields: {
-                    map_pretreatment_field: 0,
-                    _id: 0,
-                    modify_time: 0,
-                    record_time: 0,
-                    mode: 0,
-                    id: 0,
-                    status: 0
-                }
-            }
-        )
+        const detail = await spCollection.findOne(
+            query,
+            { fields: {
+                name: 1,
+                name_en: 1,
+                category_name: 1,
+                address: 1,
+                city_name: 1,
+                city_name_en: 1,
+                country_name: 1,
+                country_name_en: 1,
+                phone: 1,
+                email: 1,
+                url_web: 1,
+                photos: 1,
+                description: 1,
+                booking_info: 1
+            } }
+        );
+        if(detail) delete detail._id;
+        return detail;
     }
 }
