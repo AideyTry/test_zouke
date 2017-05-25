73ab82381baa2d422ab7ad5a60533752fa786394
wangkehan <shjyh651079151@gmail.com>
2017-05-25 21:13:51 +0800

    dashboard

--- zktravel-manage/client-src-2/app/dashboard/routes.js
+++ zktravel-manage/client-src-2/app/dashboard/routes.js
@@ -5,6 +5,11 @@ import zkhotelRoute from './zkhotel/routes';
 export default [
     {
         path: 'dashboard', component: DashBoardPage, name: 'dashboard',
+        /*
+        meta: {
+            store: { name:'app-dashboard', m(){ return { state: { test: 'abc' } }  }}
+        },
+        */
         children:[
             ...hotelMappingRoute,
             ...zkhotelRoute
