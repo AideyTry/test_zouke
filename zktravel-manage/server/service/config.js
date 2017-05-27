bbeecf99dad62dcfb91b8b6aac43f2850a271b4e
wangkehan <shjyh651079151@gmail.com>
2017-05-27 15:43:31 +0800

    collection config

--- zktravel-manage/server/service/config.js
+++ zktravel-manage/server/service/config.js
@@ -1,4 +1,6 @@
+const { DEBUG } = require('../env');
+
 module.exports = {
     zk_collection_name: 'zk_hotels',
-    sp_collection_name: 'sp_hotels_test'
+    sp_collection_name: DEBUG ? 'sp_hotels_test' : 'sp_hotels'
 }
