bac93af2ae7a06329f6c213a2cee3e371eacbff8
hebinbin <dailinbo@gmail.com>
2017-06-07 16:17:21 +0800

    6yue7ri1617

--- zktravel-manage/client-src-2/app/dashboard/hotelMapping/routes.js
+++ zktravel-manage/client-src-2/app/dashboard/hotelMapping/routes.js
@@ -1,7 +1,7 @@
 import HotelMappingPage from './HotelMapping.Page'
 export default [
     {
-        path:'hotel-mapping-proxy',name:'dashboard-hotel-mapping-proxy',redirect:{name:'dashboard-hotel-mapping',params:{provider:'vt'}},
+        path:'hotel-mapping-proxy',name:'dashboard-hotel-mapping-proxy',redirect:{name:'dashboard-hotel-mapping',params:{provider:'wait-publish'}},
     },
     {
         path:'hotel-mapping/:provider',component:HotelMappingPage,name:'dashboard-hotel-mapping',
