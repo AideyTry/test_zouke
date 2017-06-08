b2bc3c387072eb6c872e38d7aa66104dc6249434
wangkehan <shjyh651079151@gmail.com>
2017-06-08 12:50:37 +0800

    team-req

--- zktravel-manage/server/areas/api/logic/TeamRequirement.js
+++ zktravel-manage/server/areas/api/logic/TeamRequirement.js
@@ -1,5 +1,6 @@
-const dbclient = requireRoot('db');
+const BaseOfflineOrder = require('./BaseOfflineOrder');
 const compare = require('@local/compare');
+
 
 const validReqDataRule = {
     '*priority': 'priority_level',
@@ -36,33 +37,16 @@ const validReqDataRule = {
 }
 
 
-module.exports = class TeamRequirement {
-    async $getCollection(){
-        const db = await dbclient.get();
-        return await db.collection('offline_order');
-    }
+module.exports = class TeamRequirement extends BaseOfflineOrder {
     async $insert(requirement, status, creator){
-        const collection = await this.$getCollection();
-        const today = new Date().format('YYMMDD');
-        const idReg = new RegExp(`^T${today}`);
-        const maxIdResult = await collection.find({_id: idReg}, {_id: 1}).sort({ _id: -1 }).next();
-
         const date = new Date().format('YYYY-MM-DD hh:mm:ss');
         requirement.last_update = date;
 
-        const nextId = maxIdResult ? `T${today}${ 
-            (parseInt(maxIdResult._id.substr(today.length+1)) + 1).toString().padStart(3, '0')
-        }` : `T${today}001`;
-
-        await collection.insertOne({
-            _id: nextId,
-            status,
-            create_time: date,
-            last_update: date,
+        return await super.$insert({
             creator,
-            requirement
+            status,
+            requirement,
         });
-        return nextId;
     }
     validRequirement(data){
         return compare( validReqDataRule, data );
