dd496dd1378db2314679d270f36af208b8440358
wangkehan <shjyh651079151@gmail.com>
2017-06-11 18:43:42 +0800

    order/requirement

--- zktravel-manage/server/areas/api/team/controllers/OrderController.js
+++ zktravel-manage/server/areas/api/team/controllers/OrderController.js
@@ -3,6 +3,7 @@ const OfflineOrder = require('../@logic/Order');
 
 module.exports = class OfflineOrderController extends LController {
     async query(){
+        console.log(this.userInfo.roleName);
         const { page, pageSize, status = 1 } = this.request.body;
         const offlineOrder = new OfflineOrder();
         const list = await offlineOrder.query(status, { page, pageSize }); 
