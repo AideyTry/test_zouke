0b700f4f62ad05c9fc9ddd5bdf1a6f7506df5b82
hebinbin <dailinbo@gmail.com>
2017-06-19 10:14:36 +0800

    Merge branch 'changeui' of hk.zouke.com:zouke-2.0-frontend into changeui
    # Conflicts:
    #	zktravel-manage/client-src-2/app/dashboard/order-details/OrderDetail.Page.vue

--- zktravel-manage/client-src-2/app/dashboard/system-order/SystemOrder.Page.vue
+++ zktravel-manage/client-src-2/app/dashboard/system-order/SystemOrder.Page.vue
@@ -30,6 +30,25 @@
             margin: 0 5px;
         }
     }
+    /*order start */
+    .orderContent{
+        .city{
+            .el-row{
+                padding:2px 0;
+            }
+            .red{
+                color:#f00;
+            }
+            .delete{
+                color:#f00;
+
+            }
+            .delete:hover{
+                cusor:pointer;
+            }
+        }
+    }
+    /*order end*/
 </style>
 
 <template>
@@ -37,7 +56,73 @@
         <el-tabs v-model="$route.params.order" @tab-click="changeTab">
 
             <el-tab-pane label="有效订单" name="effective"></el-tab-pane>
-            <el-tab-pane label="无效订单" name="nonEffective"></el-tab-pane>
+            <el-tab-pane label="无效订单" name="nonEffective">
+                <div>
+                    <div class="orderContent">
+                        <div class="orderHotel">
+                            <el-row type="flex">
+                                <el-col :span="4">
+                                    <h4>订单信息</h4>
+                                </el-col>
+                            </el-row>
+                            <el-tabs type="border-card">
+                                <template>
+                                    <el-tab-pane>
+                                        <div class="city">
+                                            <el-row type="flex">
+                                                <el-col :span="2">
+                                                    <strong>入住/离店:</strong>
+                                                </el-col>
+                                                <el-col :span="9">
+                                                    <span></span>
+                                                    <span>晚</span>
+                                                </el-col>
+                                            </el-row>
+                                            <el-row type="flex">
+                                                <el-col :span="2"><strong>酒店<i class="red">*</i></strong></el-col>
+                                                <el-col :span="8">
+                                                    <el-autocomplete
+                                                            size="small"
+                                                            placeholder="请输入关键字选择"
+                                                    >
+
+                                                    </el-autocomplete>
+                                                </el-col>
+                                                <el-col :span="3">
+                                                    <span>B评分:</span>
+                                                    <span>{{}}</span>
+                                                    <span class="delete">删除</span>
+                                                </el-col>
+                                            </el-row>
+                                            <el-row type="flex">
+                                                <el-col :span="2">
+                                                    <strong>备注确认号:</strong>
+                                                </el-col>
+                                                <el-col :span="10">
+                                                    <el-input placeholder="确认用户备注后，填写相应的编号，用';'隔开">
+
+                                                    </el-input>
+                                                </el-col>
+                                            </el-row>
+                                            <el-row type="flex">
+                                                <el-col :span="24"></el-col>
+                                            </el-row>
+                                            <el-row type="flex">
+                                                <el-col :span="1">
+                                                    
+                                                </el-col>
+                                                <el-col :span="2">
+
+                                                </el-col>
+                                            </el-row>
+                                        </div>
+                                    </el-tab-pane>
+                                </template>
+                            </el-tabs>
+                        </div>
+                    </div>
+                </div>
+            </el-tab-pane>
 
         </el-tabs>
         <el-row type="flex" class="search-group">
