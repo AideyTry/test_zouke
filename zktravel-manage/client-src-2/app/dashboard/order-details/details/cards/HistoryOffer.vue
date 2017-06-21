c8a1c8db2218e7d2fdb3f576a8dd730deafe20a0
陈俊伟 <646945267@qq.com>
2017-06-21 15:54:55 +0800

    表单验证

--- zktravel-manage/client-src-2/app/dashboard/order-details/details/cards/HistoryOffer.vue
+++ zktravel-manage/client-src-2/app/dashboard/order-details/details/cards/HistoryOffer.vue
@@ -1,26 +1,40 @@
-<style>
-
+<style lang="scss" scoped>
+    .buttonA{
+        cursor: pointer;
+        text-underline: none;
+    }
 </style>
 <template>
     <el-table
-            :data="history"
+            :data="history.price_history"
             stripe
             style="width: 100%">
         <el-table-column
-                prop="date"
+                prop="id"
                 label="编号">
         </el-table-column>
         <el-table-column
-                prop="address"
-                label="订房员">
+                prop="check_pass"
+                label="是否通过">
+            <template scope="scope">
+                {{scope.row.check_pass?'是':'否'}}
+            </template>
         </el-table-column>
         <el-table-column
-                prop="address"
+                label="订房员">
+            <template scope="scope">
+                {{history.booking_user.name}}
+            </template>
+        </el-table-column>
+        <el-table-column
+                prop="last_update"
                 label="报价时间">
         </el-table-column>
         <el-table-column
-                prop="address"
                 label="操作">
+            <template scope="scope">
+                <a class="buttonA" @click="submitHistory(scope.row)">应用</a>
+            </template>
         </el-table-column>
     </el-table>
 </template>
@@ -29,6 +43,11 @@
         props:['history'],
         data(){
             return{}
+        },
+        methods:{
+            submitHistory(history){
+                this.$emit('useHistory',history)
+            }
         }
     }
 </script>
