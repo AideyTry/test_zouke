1d2b382a9b3e53682c43359b88b1e4835dea6fd5
qukaifei <190643280@qq.com>
2017-06-22 19:21:14 +0800

    payStream 接口

--- zktravel-manage/server/areas/api/team/@logic/OrderDetail.js
+++ zktravel-manage/server/areas/api/team/@logic/OrderDetail.js
@@ -50,16 +50,16 @@ module.exports = class OrderDetail extends BaseOrder {
                 _id: id,
                 "booking_user.id":user.id,
                 /**订单状态校验：in/nin **/
-                 status:{ $nin:[
-                    this.status.WAIT_FOR_PUBLISH,
-                    this.status.WAIT_FOR_DISPATCH,
-                    this.status.WAIT_FOR_GIVE_PRICE,
-                    this.status.WAIT_FOR_PRICE_CHECK,
-                    this.status.WAIT_FOR_PRICE_CONFIRM,
-                    this.status.WAIT_FOR_GATHERING,
-                    this.status.WAIT_FOR_ROOM_PERSON,
-                    this.status.ORDER_RESOLVE
-                ]}
+                //  status:{ $nin:[
+                //     this.status.WAIT_FOR_PUBLISH,
+                //     this.status.WAIT_FOR_DISPATCH,
+                //     this.status.WAIT_FOR_GIVE_PRICE,
+                //     this.status.WAIT_FOR_PRICE_CHECK,
+                //     this.status.WAIT_FOR_PRICE_CONFIRM,
+                //     this.status.WAIT_FOR_GATHERING,
+                //     this.status.WAIT_FOR_ROOM_PERSON,
+                //     this.status.ORDER_RESOLVE
+                // ]}
             },
             {
                 //插入数据
