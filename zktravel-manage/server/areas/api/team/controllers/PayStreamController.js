04c61f0c8b5f32c28618fd23d821a6f1b719a312
qukaifei <190643280@qq.com>
2017-06-22 15:31:00 +0800

    payStream 接口

--- zktravel-manage/server/areas/api/team/controllers/PayStreamController.js
+++ zktravel-manage/server/areas/api/team/controllers/PayStreamController.js
@@ -20,7 +20,7 @@ module.exports = class MyPayStreamController extends TeamController {
         /***
          * 进行业务逻辑之前，要根据业务需求来判断是否需要进行数据校验,不通过直接驳回请求。
          ****/
-        const result = await payStream.update(id,user,provider,extras,paytime);
+        const result = await payStream.commit(id,user,provider,extras,paytime);
         //数据库操作失败
         if(!result){
             return this.renderJSON({ code:2, msg:'can not commit pay stream' });
@@ -29,14 +29,14 @@ module.exports = class MyPayStreamController extends TeamController {
         this.renderJSON({code:0});
     };
     //录入收款信息
-    async commit(id,provider,extras,paytime){
+    async collection(id,reo,currency,money){
         const payStream = new PayStream();
         //拿到当前用户信息，更新表的时候做id校验
         let user = this.$getUser();
         /***
          * 进行业务逻辑之前，要根据业务需求来判断是否需要进行数据校验,不通过直接驳回请求。
          ****/
-        const result = await payStream.update(id,user,provider,extras,paytime);
+        const result = await payStream.collection(id,user,reo,currency,money);
         //数据库操作失败
         if(!result){
             return this.renderJSON({ code:2, msg:'can not commit pay stream' });
