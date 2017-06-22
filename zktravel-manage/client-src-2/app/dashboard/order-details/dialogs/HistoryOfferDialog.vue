3a76ff702004a7938820f4ed0b26a1aa3c75707d
陈俊伟 <646945267@qq.com>
2017-06-22 10:43:41 +0800

    历史报价

--- zktravel-manage/client-src-2/app/dashboard/order-details/dialogs/HistoryOfferDialog.vue
+++ zktravel-manage/client-src-2/app/dashboard/order-details/dialogs/HistoryOfferDialog.vue
@@ -1,9 +1,30 @@
 <style>
+    .historyDialog{
 
+    }
 </style>
 <template>
+    <div class="historyDialog">
+        <el-tabs>
+            <el-tab-pane label="历史需求" name="历史需求">
 
+            </el-tab-pane>
+            <el-tab-pane label="历史报价" name="历史报价">
+
+            </el-tab-pane>
+        </el-tabs>
+    </div>
 </template>
 <script>
+    export default{
+        props:['data'],
+        data(){
+            return{
+                orderdata:[]
+            }
+        },
+        methods:{
 
+        }
+    }
 </script>
