df9b6b2fc36b3c645b078eca6d79e1e690041bde
qukaifei <190643280@qq.com>
2017-06-22 11:05:36 +0800

    payStream 接口

---
+++ zktravel-manage/server/areas/api/team/@logic/OrderDetail.js
@@ -0,0 +1,77 @@
+const BaseOrder = require('./BaseOrder');
+const compare = require('@local/compare');
+
+const orderRule = {
+    '*country': [
+        {
+            "*hotels":[
+                {
+                    "hotel_name":"酒店名",
+                    "remark_confirm":"备注信息",
+                    "*suppliers":[
+                        {
+                            "supplier_name":"渠道名",
+                            "room_type":"",
+                            "*status":1,
+                            "*rooms":[
+                                {
+                                    "*type":"Single",
+                                    "*number":1,
+                                    "*room_description":"房间描述",
+                                    "*peoples":[
+                                        {
+                                            "*name":"名",
+                                            "*family_name":"姓",
+                                            "*gender":"性别"
+                                        },
+                                        {min:1}
+                                    ]
+                                },
+                                {min:1}
+                            ]
+                        },
+                        {min:1}
+                    ]
+                },
+                {min:1}
+            ]
+        },
+        {min: 1}
+    ]
+};
+
+module.exports = class OrderDetail extends BaseOrder {
+    //订单数据校验
+    validOrder(order){
+        return compare(orderRule, order);
+    }
+
+    //数据入库
+    async update(id,user,order){
+        /**方法前面带$的说明是从父类继承来的**/
+        return await this.$update(
+            {
+                _id: id,
+                "creator.id":user.id,
+                /**订单状态校验：in/nin **/
+                // status:{ $nin:[
+                //     this.status.WAIT_FOR_PUBLISH,
+                //     this.status.WAIT_FOR_DISPATCH,
+                //     this.status.WAIT_FOR_GIVE_PRICE,
+                //     this.status.WAIT_FOR_PRICE_CHECK,
+                //     this.status.WAIT_FOR_PRICE_CONFIRM,
+                // ]}
+            },
+            {
+                //插入数据
+                $push: {
+                    order_detail:{
+                        order_obj: order,
+                    },
+                    //日志记录
+                    logs: this.$createShiftUpdate({ type: 'user:save_order', time: this.$createTime(), user })
+                }
+            }
+        )
+    }
+};
--- zktravel-manage/server/areas/api/team/controllers/PayStreamController.js
+++ zktravel-manage/server/areas/api/team/controllers/PayStreamController.js
@@ -12,15 +12,15 @@ module.exports = class MyPayStreamController extends TeamController {
             }
         }
     }
-    //提交
+    //提交付款信息
     async commit(id,provider,extras,paytime){
+        const payStream = new PayStream();
+        //拿到当前用户信息，更新表的时候做id校验
+        let user = this.$getUser()
         /***
          * 进行业务逻辑之前，要根据业务需求来判断是否需要进行数据校验,不通过直接驳回请求。
          ****/
-
-        //业务逻辑
-        const payStream = new PayStream();
-        const result = await payStream.update(id,provider,extras,paytime);
+        const result = await payStream.update(id,user,provider,extras,paytime);
         //数据库操作失败
         if(!result){
             return this.renderJSON({ code:2, msg:'can not commit pay stream' });
