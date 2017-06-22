2c1c27ad4aeebe15f34a22d8c5f8bf1c257bc877
hebinbin <dailinbo@gmail.com>
2017-06-22 09:54:01 +0800

    Merge branch 'changeui' of hk.zouke.com:zouke-2.0-frontend into changeui
    # Conflicts:
    #	zktravel-manage/client-src-2/store.js

---
+++ zktravel-manage/client-src-2/app/dashboard/order-details/details/cards/HotelsCard.vue
@@ -0,0 +1,149 @@
+<style lang="scss" scoped>
+
+</style>
+<template>
+    <div>
+        <el-form v-for="(v,index) in item" :key="v.suppliers" :model="myItem">
+                <el-row type="flex">
+                    <el-col :span="2"><strong>酒店<i class="red">*</i></strong></el-col>
+                    <el-col :span="8">
+                        <el-autocomplete
+                                size="small"
+                                v-model="hotel"
+                                :fetch-suggestions="searchHotel"
+                                placeholder="请输入关键字选择"
+                                @select="selectHotel"
+                        >
+
+                        </el-autocomplete>
+                    </el-col>
+                    <el-col :span="3">
+                        <span>B评分:</span>
+                        <span>{{}}</span>
+                    </el-col>
+                    <el-col :span="2">
+                        <el-button type="text" class="delete" @click="deleteHotel()">删除</el-button>
+                    </el-col>
+                    <el-col :span="2">
+                        <el-button type="text" @click="addHotel()">添加酒店</el-button>
+                    </el-col>
+                </el-row>
+                <el-row type="flex">
+                    <el-col :span="2">
+                        <strong>备注确认号:</strong>
+                    </el-col>
+                    <el-col :span="10">
+                        <el-input v-model="input" placeholder="确认用户备注后，填写相应的编号，用';'隔开">
+
+                        </el-input>
+                    </el-col>
+                </el-row>
+                <!--添加供应商start-->
+                <el-row type="flex" >
+                    <el-col :span="20">
+                        <!--<el-button v-if="(isTrue_supplier==false)" type="text" @click="addSupplier()">添加供应商</el-button>-->
+                        <AddSupplierCard  :currentData="params" :item="v.suppliers"></AddSupplierCard>
+                    </el-col>
+                </el-row>
+                <!--添加供应商end-->
+        </el-form>
+    </div>
+</template>
+<script>
+    import ajax from '@local/common/ajax';
+    import debounce from 'lodash/debounce';
+    import AddSupplierCard from './AddSupplierCard'
+    export default{
+        props:["items","item","index","params","orders"],
+        data(){
+            return{
+                myItem:this.items,
+                hotel:'',
+                hotelFlag:false,
+                input:''
+            }
+        },
+        components:{
+            AddSupplierCard:AddSupplierCard
+        },
+        computed:{
+            count(){
+                return this.$store.getters.count;
+            }
+        },
+        methods:{
+
+            /*search start*/
+            searchHotel:debounce(function (queryString, cb) {
+                if (queryString) {
+                    ajax.postSilence('/api/hotel/zk-hotel/search', {
+                        keyword: queryString.trim()
+                    }).then(
+                        data => {
+                            let arr = []
+                            if(data.list){
+                                data.list.forEach(
+                                    (v, k) => {
+                                        arr.push({value: v.name, item: v})
+                                    }
+                                )
+                                cb(arr)
+                            }
+
+                        }
+                    )
+                }
+            }, 1000),
+            selectHotel(item){
+                let arr = item.item;
+                this.hotelflag = true;
+                this.myItem.hotel = arr;
+
+            },
+            /*search end*/
+            addHotel(){
+                this.item.push({
+                    suppliers:[
+                        {
+                            status:this.count,
+                            rooms:[
+                                {
+                                    type:'Single',
+                                    number:1,
+                                    roomDescription:'',
+                                    peoples:[
+                                        {
+                                            name:'w',
+                                            familyName:'',
+                                            gender:''
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]});
+                this.$commit("addCount");
+            },
+            deleteHotel(){
+                if(this.item.length>1){
+                    this.item.pop();
+                    this.$commit("deleteCount");
+                }
+            }
+        },
+        watch:{
+            hotel(val){
+                if (!this.hotelflag) {
+                    this.myItem.hotel = {name: val, custom: true}
+                }
+                this.hotelflag = false;
+            }
+        },
+        created(){
+            console.log(this.items);
+//            if (this.items.hotel.name) {
+//                this.hotel = this.items.hotel.name;
+//            }
+        }
+    }
+</script>
