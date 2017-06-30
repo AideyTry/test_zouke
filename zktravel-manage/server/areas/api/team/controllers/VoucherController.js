3ebbd8d98adcec7da98d84dfd9bd953711a42d75
wangkehan <shjyh651079151@gmail.com>
2017-06-30 14:45:03 +0800

    voucher

--- zktravel-manage/server/areas/api/team/controllers/VoucherController.js
+++ zktravel-manage/server/areas/api/team/controllers/VoucherController.js
@@ -3,6 +3,21 @@ const { DEBUG } = requireRoot('env');
 
 module.exports = class VoucherController extends TeamController {
     async download(){
+        const data ={
+            checkIn: '2017-06-30',
+            checkOut: '2017-07-01',
+            confirmNumber: '123',
+            guestName: 'xxx',
+            hotelAddress: 'address',
+            hotelName: 'hotelName',
+            hotelPhone: '0000',
+            orderId: 'xxx',
+            referentNo: 'rrr',
+            roomList: [{ desc: '3 x Single', num: '3'}],
+            roomNumbers: 1,
+            hasBoard: true
+        }
+
         await this.renderRemote({ 
             host:DEBUG?'wx.test.zouke.com':'w.zouke.com', 
             path:'/zksystem/order/view-voucher',
@@ -10,20 +25,7 @@ module.exports = class VoucherController extends TeamController {
             headers:{
                 'content-type':'application/json'
             },
-            data:{
-                checkIn: '2017-06-30',
-                checkOut: '2017-07-01',
-                confirmNumber: '123',
-                guestName: 'xxx',
-                hotelAddress: 'address',
-                hotelName: 'hotelName',
-                hotelPhone: '0000',
-                orderId: 'xxx',
-                referentNo: 'rrr',
-                roomList: [{ desc: '3 x Single', num: '3'}],
-                roomNumbers: 1,
-                hasBoard: true
-            }
-        })
+            data
+        });
     }
 }
