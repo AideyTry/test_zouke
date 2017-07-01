681a2eef52923771449f1c1e1d34420ec8c5d078
qukaifei <190643280@qq.com>
2017-07-01 14:46:47 +0800

    需要开票接口优化

--- zktravel-manage/server/areas/api/team/controllers/VoucherController.js
+++ zktravel-manage/server/areas/api/team/controllers/VoucherController.js
@@ -1,5 +1,7 @@
 const TeamController = require('../TeamController');
 const { DEBUG } = requireRoot('env');
+const Voucher = require('../@logic/Voucher');
+
 
 module.exports = class VoucherController extends TeamController {
     $meta(){
@@ -15,30 +17,18 @@ module.exports = class VoucherController extends TeamController {
         }
     }
 
-    async download(id){
-        const data ={
-            checkIn: '2017-06-30',
-            checkOut: '2017-07-01',
-            confirmNumber: '123',
-            guestName: 'xxx',
-            hotelAddress: 'address',
-            hotelName: 'hotelName',
-            hotelPhone: '0000',
-            orderId: 'xxx',
-            referentNo: 'rrr',
-            roomList: [{ desc: '3 x Single', num: '3'}],
-            roomNumbers: 1,
-            hasBoard: true
-        }
+    async toDownload(orderId,orderIndex=0,hotelIndex=0,suppliersIndex=0){
+        const voucher = new Voucher();
+        const voucherDetail = await voucher.download(orderId,orderIndex,hotelIndex,suppliersIndex);
 
-        await this.renderRemote({ 
-            host:DEBUG?'wx.test.zouke.com':'w.zouke.com', 
+        await this.renderRemote({
+            host:DEBUG?'wx.test.zouke.com':'w.zouke.com',
             path:'/zksystem/order/view-voucher',
             method:'POST',
             headers:{
                 'content-type':'application/json'
             },
-            data
+            voucherDetail
         });
     }
-}
+};
