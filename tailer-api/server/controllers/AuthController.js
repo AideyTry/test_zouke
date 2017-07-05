cda5662f1a72b7d3968020602f439ad2b1d6e8c2
wangkehan <shjyh651079151@gmail.com>
2017-07-05 15:33:27 +0800

    反查

--- tailer-api/server/controllers/AuthController.js
+++ tailer-api/server/controllers/AuthController.js
@@ -2,7 +2,7 @@ const Controller = require('@local/koa-mvc/Controller');
 
 module.exports = class HomeController extends Controller {
     // @POST
-    login(id){
-        this.renderJSON({ code:0, id })
+    login(code, rawData, signature, encryptedData, iv){
+        
     }
 }
