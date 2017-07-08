a45494c4703df741e3a260e23a5083b9760dad28
wangkehan <shjyh651079151@gmail.com>
2017-07-08 23:31:58 +0800

    a

--- zktravel-manage/server/areas/api/team/@logic/AllotList.js
+++ zktravel-manage/server/areas/api/team/@logic/AllotList.js
@@ -8,8 +8,7 @@ module.exports = class AllotList extends BaseOrder {
         const collection = await this.$getCollection();
         //看该订单是否已付款
         const order = await collection.findOne({
-            '_id': id,
-            'pay_stream': null
+            '_id': id
         }, {_id:1, pay_stream:1, status:1});
 
         if(!order) return false;
@@ -26,7 +25,7 @@ module.exports = class AllotList extends BaseOrder {
         };
 
         //如果有支付信息，更改status
-        if((!order.pay_stream||order.pay_stream.length===0)&&order.status!==this.status.ORDER_RESOLVE){
+        if(order.pay_stream&&order.pay_stream.length>0&&order.status!==this.status.ORDER_RESOLVE){
             update.$set.status = this.status.WAIT_FOR_BOOKING;
         }
 
